a5daff45f05890cf379e2fdb352d8439eebed7eb
Santiihoyos <santiago.hoyos93@gmail.com>
2016-03-07 13:26:54 +0100

    Hola bebes from Santi
    Prueba comit brnace 1

---
+++ AppCalculatorOfMonsters/ViewModels/ViewModelMainPage.cs
@@ -0,0 +1,28 @@
+﻿using AppCalculatorOfMonsters.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCalculatorOfMonsters.ViewModels
+{
+    public class ViewModelMainPage
+    {
+
+        ObservableCollection<Monstruo> monstruos { get; set; }
+        
+        public ViewModelMainPage()
+        {
+           
+        }
+
+        private ObservableCollection<Monstruo> rellenado() {
+
+
+
+            return null;
+        }
+    }
+}
